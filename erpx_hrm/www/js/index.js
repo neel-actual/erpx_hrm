abf86845d0d0c3e6c39c025c9c4b11fb25e8fceb
neel <neel2292@gmail.com>
2019-09-04 22:57:24 +0800

    Login functionality

--- erpx_hrm/www/js/index.js
+++ erpx_hrm/www/js/index.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
     var $navs = $('#nav-mobile').children().clone();
 
     $('#slide-out').append($navs);
+
+
+    window.logout = function() {
+		return frappe.call({
+			method:'logout',
+			callback: function(r) {
+				if(r.exc) { return; }
+				window.location.href = '/';
+			}
+		});
+	}
 });
