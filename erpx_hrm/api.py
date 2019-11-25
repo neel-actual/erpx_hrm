741e819b1df3c9601f884c31d8438c48c4e76ab4
Vincent Nguyen <vinhnguyen.t090@gmail.com>
2019-11-25 09:20:40 +0000

    server hr claim

--- erpx_hrm/api.py
+++ erpx_hrm/api.py
@@ -181,4 +181,37 @@ def delete_expense_type(doctype,name):
     frappe.delete_doc(doctype, name, ignore_missing=False)
     return "Expense Claim Deleted!"
 
+@frappe.whitelist()
+def get_approvers(employee,doctype):
+    # frappe.throw("here")
+
+	# if not employee:
+	# 	frappe.throw(_("Please select Employee Record first."))
+
+    approvers = []
+    department_details = {}
+    department_list = []
+    employee_department = frappe.get_value("Employee", employee, "department")
+    if employee_department:
+        department_details = frappe.db.get_value("Department", {"name": employee_department}, ["lft", "rgt"], as_dict=True)
+    if department_details:
+        department_list = frappe.db.sql("""select name from `tabDepartment` where lft <= %s
+            and rgt >= %s
+            and disabled=0
+            order by lft desc""", (department_details.lft, department_details.rgt), as_list = True)
+
+    if doctype == "Leave Application":
+        parentfield = "leave_approvers"
+    else:
+        parentfield = "expense_approvers"
+    if department_list:
+        for d in department_list:
+            approvers += frappe.db.sql("""select user.name from
+                tabUser user, `tabDepartment Approver` approver where
+                approver.parent = %s
+                and approver.parentfield = %s
+                and approver.approver=user.name""",(d, parentfield), as_list=True)
+
+    return approvers
+
 
