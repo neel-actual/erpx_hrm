b1b650ce9e5400b5a615557bf2af7589a8c5193b
Vincent Nguyen <vinhbk2000@gmail.com>
2019-12-02 08:07:28 +0700

    Organization - Department

--- erpx_hrm/www/app-assets/js/custom/custom-script.js
+++ erpx_hrm/www/app-assets/js/custom/custom-script.js
@@ -12,8 +12,8 @@ WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR CUSTOM SCRIPT I
 
 frappe.provide("frappe");
 $.extend(frappe, {
-	ajax: function(opts) {
-		if (typeof arguments[0]==='string') {
+	ajax: function (opts) {
+		if (typeof arguments[0] === 'string') {
 			opts = {
 				url: arguments[0],
 				args: arguments[1],
@@ -30,36 +30,36 @@ $.extend(frappe, {
 			dataType: "json",
 			headers: { "X-Frappe-CSRF-Token": frappe.csrf_token },
 			statusCode: opts.statusCode || {
-				404: function() {
+				404: function () {
 					M.toast({
 						html: __("Not found")
 					})
 				},
-				403: function() {
+				403: function () {
 					M.toast({
 						html: __("Not permitted")
 					})
 				},
-				202: function() {
-					if(opts.callback)
+				202: function () {
+					if (opts.callback)
 						opts.callback();
-					if(opts.success)
+					if (opts.success)
 						opts.success();
 				},
-				200: function(data) {
-					if(opts.callback)
+				200: function (data) {
+					if (opts.callback)
 						opts.callback(data);
-					if(opts.success)
+					if (opts.success)
 						opts.success(data);
 				}
 			}
-		}).always(function(data) {
-			if(opts.freeze) {
+		}).always(function (data) {
+			if (opts.freeze) {
 				frappe.unfreeze();
 			}
 
 			// executed before statusCode functions
-			if(data.responseText) {
+			if (data.responseText) {
 				try {
 					data = JSON.parse(data.responseText);
 				} catch (e) {
@@ -69,39 +69,39 @@ $.extend(frappe, {
 			frappe.process_response(opts, data);
 		});
 	},
-	prepare_call_ajax: function(opts) {
-		if(opts.btn) {
+	prepare_call_ajax: function (opts) {
+		if (opts.btn) {
 			$(opts.btn).prop("disabled", true);
 		}
 
-		if(opts.msg) {
+		if (opts.msg) {
 			$(opts.msg).toggle(false);
 		}
 
-		if(!opts.args) opts.args = {};
+		if (!opts.args) opts.args = {};
 
 		// method
-		if(opts.method) {
+		if (opts.method) {
 			opts.args.cmd = opts.method;
 		}
 
 		// stringify
-		opts.args = {"data":JSON.stringify(opts.args)};
+		opts.args = { "data": JSON.stringify(opts.args) };
 
-		if(!opts.no_spinner) {
+		if (!opts.no_spinner) {
 			//NProgress.start();
 		}
 	},
-    process_response: function(opts, data) {
+	process_response: function (opts, data) {
 		//if(!opts.no_spinner) NProgress.done();
 
-		if(opts.btn) {
+		if (opts.btn) {
 			$(opts.btn).prop("disabled", false);
 		}
 
 		if (data._server_messages) {
 			var server_messages = JSON.parse(data._server_messages || '[]');
-			server_messages = $.map(server_messages, function(v) {
+			server_messages = $.map(server_messages, function (v) {
 				// temp fix for messages sent as dict
 				try {
 					return JSON.parse(v).message;
@@ -110,7 +110,7 @@ $.extend(frappe, {
 				}
 			}).join('<br>');
 
-			if(opts.error_msg) {
+			if (opts.error_msg) {
 				$(opts.error_msg).html(server_messages).toggle(true);
 			} else {
 				M.toast({
@@ -119,25 +119,25 @@ $.extend(frappe, {
 			}
 		}
 
-		if(data.exc) {
+		if (data.exc) {
 			try {
 				var err = JSON.parse(data.exc);
-				if($.isArray(err)) {
+				if ($.isArray(err)) {
 					err = err.join("\n");
 				}
 				console.error ? console.error(err) : console.log(err);
-			} catch(e) {
+			} catch (e) {
 				console.log(data.exc);
 			}
 
-		} else{
+		} else {
 
 		}
-		if(opts.msg && data.message) {
+		if (opts.msg && data.message) {
 			$(opts.msg).html(data.message).toggle(true);
 		}
 
-		if(opts.always) {
+		if (opts.always) {
 			opts.always(data);
 		}
 	}
@@ -147,97 +147,105 @@ $.extend(frappe, {
 frappe.provide("xhrm.views");
 
 xhrm.views.ListCRUD = Class.extend({
-	init: function(opts) {
-        $.extend(this, opts);
+	init: function (opts) {
+		$.extend(this, opts);
 		this.get_list();
-    },
-    create_doc: function(args) {
-        var me = this;
-        frappe.ajax({
-            url: `/api/resource/${me.doctype}`,
-            args: args,
-            callback: function(r){
-                if (!r.exc) {
-                    M.toast({
-                        html: "Added Successfully!"
-                    })
-                    me.get_list();
-                }
-            }
-        });
 	},
-	rename_doc: function(args) {
-        var me = this;
-        frappe.call({
-            method: "frappe.model.rename_doc.rename_doc",
-            args: args,
-            callback: function(r){
-                if (!r.exc) {
-                    me.get_list();
-                }
-            }
-        });
-    },
-	update_doc: function(args) {
-        var me = this;
-        frappe.ajax({
+	get_doc: function (name) {
+		var me = this;
+		return frappe.ajax({
+			type: "GET",
+			url: `/api/resource/${me.doctype}/${name}`
+		});
+	},
+	create_doc: function (args) {
+		var me = this;
+		frappe.ajax({
+			url: `/api/resource/${me.doctype}`,
+			args: args,
+			callback: function (r) {
+				if (!r.exc) {
+					M.toast({
+						html: "Added Successfully!"
+					})
+					me.get_list();
+				}
+			}
+		});
+	},
+	rename_doc: function (args) {
+		var me = this;
+		frappe.call({
+			method: "frappe.model.rename_doc.rename_doc",
+			args: args,
+			callback: function (r) {
+				if (!r.exc) {
+					me.get_list();
+				}
+			}
+		});
+	},
+	update_doc: function (name, args) {
+		var me = this;
+		frappe.ajax({
 			type: "PUT",
-            url: `/api/resource/${me.doctype}`,
-            args: args,
-            callback: function(r){
-                if (!r.exc) {
-                    M.toast({
-                        html: "Updated Successfully!"
-                    })
-                    me.get_list();
-                }
-            }
-        });
-    },
-    get_list: function() {
+			url: `/api/resource/${me.doctype}/${name}`,
+			args: args,
+			callback: function (r) {
+				if (!r.exc) {
+					M.toast({
+						html: "Updated Successfully!"
+					})
+					me.get_list();
+				}
+			}
+		});
+	},
+	get_list: function () {
 		var me = this;
 		frappe.call({
 			method: "frappe.client.get_list",
 			args: {
 				doctype: me.doctype,
+				fields: ["*"],
 				limit_page_length: 0
 			},
-			callback: function(r){
+			callback: function (r) {
 				if (!r.exc) {
 					me.render_list(r.message);
 				}
-            }
+			}
 		});
-    },
-    render_list: function(data) {
-        var me = this;
-        let html = '';
+	},
+	render_list: function (data) {
+		var me = this;
+		let html = '';
 		if (data.length) {
-			let get_item_html = item => {
-                return `
-                    <li class="list-item">
-						<span>${item.name}
-						<a href="#" class="modal-trigger btn-delete" data-name="${item.name}">
-							<img class="img-del-dep" src="/icons/icon-58.png" width="21" height="21">
-                      	</a>
-                        <a style="float: right; padding-right: 10px;" href="#modal-rename-job" class="btn-rename modal-trigger" data-name="${item.name}" data-modal="modal-rename-job">Edit</a>
-                        </span>
-                    </li>
-                `;
-			};
-			html = data.map(get_item_html).join('');
+			html = data.map(me.get_item_html).join('');
 		} else {
 			html = `<li class="text-center">
 					<span class="text-muted">${__('No Item')}</span>
 				</li>`;
 		}
-        me.parent.html(`<ul class="content-width">${html}</ul>`);
-        me.bind_event();
-    },
-    bind_event: function() {
+		me.parent.html(`<ul class="content-width">${html}</ul>`);
+		me.bind_event();
+	},
+	get_item_html: function (item) {
+		return `
+			<li class="list-item">
+				<span>${item.name}
+				<a href="#" class="modal-trigger btn-delete" data-name="${item.name}">
+					<img class="img-del-dep" src="/icons/icon-58.png" width="21" height="21">
+				</a>
+				<a style="float: right; padding-right: 10px;" href="#modal-rename-job" class="btn-rename modal-trigger" data-name="${item.name}" data-modal="modal-rename-job">Edit</a>
+				</span>
+			</li>
+		`;
+	},
+	bind_event: function () {
 		var me = this;
-		me.parent.find(".btn-delete").click(function(){
-			var name = $(this).attr("data-name"); 
+		me.parent.find(".btn-delete").click(function () {
+			var name = $(this).attr("data-name");
 			var row = $(this).closest(".list-item");
 			swal({
 				title: "Are you sure you want to delete?",
@@ -247,26 +255,81 @@ xhrm.views.ListCRUD = Class.extend({
 					delete: 'Yes, Delete It'
 				}
 			}).then(function (result) {
-				if(result){
+				if (result) {
 					frappe.ajax({
 						type: "DELETE",
 						url: `/api/resource/${me.doctype}/${name}`,
-						callback: function(r){
+						callback: function (r) {
 							M.toast({
 								html: __("Deleted Successfully!")
 							});
 							row.remove();
 						}
-					});	
+					});
 				}
 			})
 			return false;
 		});
-		me.parent.find(".btn-rename").click(function(){
+		me.parent.find(".btn-rename").click(function () {
 			var name = $(this).attr("data-name");
-			var modal = $("#"+$(this).attr("data-modal"));
+			var modal = $("#" + $(this).attr("data-modal"));
 			modal.find("#old_name").val(name);
 			modal.find("#new_name").val(name);
 		});
-    }
-});    
+	}
+});
+
+frappe.provide("xhrm.utils");
+
+xhrm.utils.optionJSON = function (self, newOptions, selectedOption) {
+	var self = $(self);
+	if (self.prop)
+		var options = self.prop('options');
+	else
+		var options = self.attr('options');
+
+	$('option', self).remove();
+	$.each(newOptions, function (val, text) {
+		options[options.length] = new Option(text, val);
+	});
+	self.val(selectedOption);
+};
+
+
+xhrm.utils.optionArray = function (self, newOptions, selectedOption) {
+	var self = $(self);
+	if (self.prop)
+		var options = self.prop('options');
+	else
+		var options = self.attr('options');
+
+	$('option', self).remove();
+
+	if (options != undefined) {
+		$.each(newOptions, function (index, option) {
+			options[options.length] = new Option(option.value, option.key);
+		});
+		self.val(selectedOption);
+	}
+};
+
+window.__ = function(txt, replace) {
+	if(!txt)
+		return txt;
+	if(typeof(txt) != "string")
+		return txt;
+	var ret = frappe._messages[txt.replace(/\n/g, "")] || txt;
+	if(replace && typeof(replace) === "object") {
+		ret = txt.format(replace);
+	}
+	return ret;
+};
+
+String.prototype.format = function() {
+	var str = this;
+	for (var i = 0; i < arguments.length; i++) {       
+	  var reg = new RegExp("\\{" + i + "\\}", "gm");             
+	  str = str.replace(reg, arguments[i]);
+	}
+	return str;
+  }
