dcb08eb56f47dc6c6e5f10ab0667852f49c2133e
Vincent Nguyen <vinhbk2000@gmail.com>
2020-01-05 19:31:09 +0700

    Account Settings - Birthday Reminders

--- erpx_hrm/www/acc_settings.py
+++ erpx_hrm/www/acc_settings.py
@@ -8,7 +8,10 @@ def get_context(context):
 
     context.user = frappe.session.user
     context.csrf_token = frappe.sessions.get_csrf_token()
+    
     doc_user = frappe.get_doc("User", frappe.session.user)
     context.user_full_name = doc_user.full_name
+
+    context.stop_birthday_reminders = frappe.db.get_single_value("HR Settings", "stop_birthday_reminders") or 0
 
     return context
