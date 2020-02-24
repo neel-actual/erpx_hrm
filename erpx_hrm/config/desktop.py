0f2ce5197fe82ca8eaa539a0c7bc6cc2c956e926
neel <neel2292@gmail.com>
2020-02-24 00:41:30 +0000

    minor change

--- erpx_hrm/config/desktop.py
+++ erpx_hrm/config/desktop.py
@@ -3,12 +3,4 @@ from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
-	return [
-		{
-			"module_name": "ERPX Human Resource Module",
-			"color": "#ffca28",
-			"icon": "octicon octicon-organization",
-			"type": "module",
-			"label": _("ERPX Human Resource Module")
-		}
-	]
+	return []
