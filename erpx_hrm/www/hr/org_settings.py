b7886d75b8be7cd8f1e8b573d0557dca88e82db6
Unknown <daniel82hien@gmail.com>
2020-04-04 21:47:51 +0700

    Add hard code permission: HR Hub

--- erpx_hrm/www/hr/org_settings.py
+++ erpx_hrm/www/hr/org_settings.py
@@ -9,6 +9,16 @@ def get_context(context):
         frappe.local.flags.redirect_location = '/'
         raise frappe.Redirect
 
+    valid_roles = ['HR Manager'] 
+    if not ("HR Manager" in frappe.get_roles()): 
+        frappe.throw(_('Only users with {0} role can access').format(', '.join(valid_roles)), 
+      frappe.PermissionError) 
+    
+    valid_roles = ['HR Manager'] 
+    if not ("HR Manager" in frappe.get_roles()): 
+        frappe.throw(_('Only users with {0} role can access').format(', '.join(valid_roles)), 
+      frappe.PermissionError) 
+    
     context.user = frappe.session.user
     context.csrf_token = frappe.sessions.get_csrf_token()
 
