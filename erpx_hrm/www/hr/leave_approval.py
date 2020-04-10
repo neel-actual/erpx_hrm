33b9dbb338a1ec71a7bf7e65bd7802737930e4ff
Vincent Nguyen <vinhbk2000@gmail.com>
2020-04-10 15:09:35 +0700

    leave approval filter by user login

--- erpx_hrm/www/hr/leave_approval.py
+++ erpx_hrm/www/hr/leave_approval.py
@@ -13,7 +13,7 @@ def get_context(context):
 
     valid_roles = ['Leave Approver']
     
-    if not frappe.utils.is_subset(valid_roles, frappe.get_roles()):
+    if not('Leave Approver' in frappe.get_roles() or 'HR Manager' in frappe.get_roles()):
         frappe.throw(_('Only users with {0} role can access').format(', '.join(valid_roles)),
 			frappe.PermissionError)
     
@@ -21,15 +21,19 @@ def get_context(context):
 		select la.*, f.file_name, f.file_url
         from `tabLeave Application` la
         left join `tabFile` f on f.attached_to_doctype = "Leave Application" and f.attached_to_name = la.name
-		where la.docstatus = 0
-	""", as_dict=True)
+		where la.docstatus = 0 and la.leave_approver = %(user)s
+	""",{
+        "user": frappe.session.user
+    }, as_dict=True, debug=1)
 
     context.leave_historys = frappe.db.sql("""
 		select la.*, f.file_name, f.file_url
         from `tabLeave Application` la
         left join `tabFile` f on f.attached_to_doctype = "Leave Application" and f.attached_to_name = la.name
-		where la.docstatus = 1
-	""", as_dict=True)
+		where la.docstatus = 1 and la.leave_approver = %(user)s
+	""",{
+        "user": frappe.session.user
+    }, as_dict=True)
 
     context.list_employee = frappe.db.get_all("Employee",fields=["name", "employee_name"])
     context.list_leave_type = frappe.db.get_all("Leave Type",fields=["name"])
