4abffbc59989b3346d29b39846ee34feddf86d76
Unknown <daniel82hien@gmail.com>
2020-04-10 11:54:40 +0700

    Setting->Group->Edit people => working

---
+++ erpx_hrm/www/hr/grp_settings.py
@@ -0,0 +1,32 @@
+import frappe
+import json
+
+def get_context(context):
+    if frappe.session.user == 'Guest':
+        frappe.local.flags.redirect_location = '/'
+        raise frappe.Redirect
+
+    context.user = frappe.session.user
+    context.csrf_token = frappe.sessions.get_csrf_token()
+
+    #Role list
+    role_arr = ["Employee", "Expense Verified", "Expense Approver", "HR Manager", "Leave Approver"]
+    role_list = []    
+    for i_role in role_arr:                        
+        user_list = frappe.db.sql_list("""
+							select u.name from `tabUser` u
+                            left join `tabHas Role` r ON  r.parent = u.name
+                            where r.role = %(role_name)s""", {"role_name": i_role})                  
+        role_list.append({
+            "name": i_role,
+            "total_user": len(user_list) or 0,
+            "user_list": user_list
+        })            
+    context.role_list = role_list
+
+    #User list
+    context.all_user = frappe.db.sql_list("""
+							select u.name from `tabUser` u
+                            left join `tabHas Role` r ON  r.parent = u.name
+                            """)     
+    return context
