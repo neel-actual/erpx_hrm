a65dff6cdf0fa7d5b24d8d8eb7669d0ec384d332
Unknown <daniel82hien@gmail.com>
2020-04-13 19:54:44 +0700

    Hr->People->About me: add/edit.

--- erpx_hrm/www/hr/js/people.js
+++ erpx_hrm/www/hr/js/people.js
@@ -34,7 +34,36 @@ $("#btn-save-employee").click(function () {
         args: args,
         callback: function (r) {
             if (!r.exc_type) {                
-                //location.reload();
+                location.reload();
+            }
+        }
+    });
+});
+
+$("#btn-save-about-me").click(function () {
+    var args = {};
+    var fields = [
+        "bio",
+        "favorites"        
+    ]
+    $.each(fields, function (key, element) {			        
+        args[element] = $(`#divAboutMe [data-fieldname="${element}"]`).val();   						
+    });    
+
+    var url = '/api/resource/Employee';    
+    var type = 'POST';
+    var old_name = $('#divBasicInfo #old_name').val();
+    if(old_name != ''){
+        url += '/' + old_name;
+        type = 'PUT';
+    }
+    frappe.ajax({
+        type: type,
+        url: url,
+        args: args,
+        callback: function (r) {
+            if (!r.exc_type) {                
+                location.reload();
             }
         }
     });
